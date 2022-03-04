d62ad5ad12af0d8fc1129fcb8a29e7d458c9ab65
UnicycleUnicorn <brand227796@gmail.com>
2022-03-03 23:16:15 -0500

    First Commit of NewAccount page
    	- There is a comment in newaccountpage.jsp where the model/ database
    should be updated and the user will need to be auto-logged in

--- Game Repository/src/Main/Main.java
+++ Game Repository/src/Main/Main.java
@@ -11,18 +11,18 @@ public class Main {
 		Launcher launcher = new Launcher();
 		
 		// get a server for port 8081
-		System.out.println("CREATING: web server on port 8081");
-		Server server = launcher.launch(true, 8081, warFile.getAbsolutePath(), "/gamerepo");
+		System.out.println("CREATING: web server on port 8080");
+		Server server = launcher.launch(true, 8080, warFile.getAbsolutePath(), "/gamerepo");
 
         // Start things up!		
-		System.out.println("STARTING: web server on port 8081");
+		System.out.println("STARTING: web server on port 8080");
 		server.start();
 		
 		// dump the console output - this will produce a lot of red text - no worries, that is normal
 		server.dumpStdErr();
 		
 		// Inform user that server is running
-		System.out.println("RUNNING: web server on port 8081");
+		System.out.println("RUNNING: web server on port 8080");
 		
         // The use of server.join() the will make the current thread join and
         // wait until the server is done executing.
