4070ad6fdefcf940bfb63875ddedf6bedcacbdd0
pwalter1014 <pwalter3@ycp.edu>
2022-03-14 00:44:08 -0400

    added css to stat page
    unfinished. need to add method of switching text for local stats

--- Game Repository/src/Servlets/StatisticsPageServlet.java
+++ Game Repository/src/Servlets/StatisticsPageServlet.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class StatisticsPageServelet extends HttpServlet {
+public class StatisticsPageServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		System.out.println("Login Servlet: doGet");
+		System.out.println("Statistics Servlet: doGet");
 		
 		req.getRequestDispatcher("_view/statpage.jsp").forward(req, resp);
 	}
