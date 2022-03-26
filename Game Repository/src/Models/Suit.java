b53aeda3b1547674504bab42e8d244ee112ea386
pwalter1014 <pwalter3@ycp.edu>
2022-03-26 00:12:45 -0400

    changed chars to strings
    this is temporary. just want to make good implementation of blackjack

--- Game Repository/src/Models/Suit.java
+++ Game Repository/src/Models/Suit.java
@@ -1,32 +1,32 @@
 package Models;
 
 public enum Suit {
-	CLUBS('c', Color.BLACK),
-	DIAMONDS('d', Color.RED),
-	HEARTS('h', Color.RED),
-	SPADES('s', Color.BLACK);
+	CLUBS("♣", Color.BLACK),
+	DIAMONDS("♦", Color.RED),
+	HEARTS("♥", Color.RED),
+	SPADES("♠", Color.BLACK);
 	
-	private final char symbol;
+	private final String symbol;
 	private final Color color;
 	
 	//Constructor
-	private Suit(char symbol, Color color) {
+	private Suit(String symbol, Color color) {
 		this.symbol = symbol;
 		this.color = color;
 	}
 	
 	//getter
-	public char getSymbol() {
-		return symbol;
+	public Color getColor() {
+		return color;
 	}
 	
-	public char toChar() {
+	public String toString() {
 		return symbol;
 	}
 	
 	//To retrieve the name of the enumeration as a string
 	
-	/*public char getMemberName() {
-		return super.toChar();
-	}*/
+	public String getMemberName() {
+		return super.toString();
+	}
 }
