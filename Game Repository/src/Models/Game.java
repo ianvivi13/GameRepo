01b05acbf466fe7773cf466523f6438f3f64c446
pwalter1014 <pwalter3@ycp.edu>
2022-03-27 17:23:48 -0400

    starter implementation of game

--- Game Repository/src/Models/Game.java
+++ Game Repository/src/Models/Game.java
@@ -1,11 +1,32 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class Game {
 	
-	private ArrayList<int> players;
+	private ArrayList<Integer> players;
 	private String gameID;
+	private TurnOrder tko;
+	
 	
 	public Game(){
+		players = new ArrayList<>();
+		gameID = " ";
+	}
+	
+	public void nextTurn() {
+		tko.NextTurn();
+	}
+	
+	public void addPlayer(String player) {
+		tko.AddPlayer(player);
+	}
+	
+	public void removePlayer(String player) {
+		tko.RemovePlayer(player);
+	}
+	
+	public void updateStats() {
 		
 	}
 }
