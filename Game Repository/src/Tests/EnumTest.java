c6c3e92a6959d938f9f93169774d7e532a5dff49
pwalter1014 <pwalter3@ycp.edu>
2022-04-06 00:18:08 -0400

    added enum test
    needs to be implemented

---
+++ Game Repository/src/Tests/EnumTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Models.StandardCard;
+import Models.Rank;
+import Models.Suit;
+
+public class EnumTest{
+	
+	
+	
+}
