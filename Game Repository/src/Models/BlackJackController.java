6f9e2306304076c052fb967778707137c4e1142b
pwalter1014 <pwalter3@ycp.edu>
2022-04-22 16:09:16 -0400

    80% implementation of BLJ controller

--- Game Repository/src/Models/BlackJackController.java
+++ Game Repository/src/Models/BlackJackController.java
@@ -12,45 +12,60 @@ import Database.elves.InitDatabase;
 
 public class BlackJackController extends Game {
 	
+	Game blj = new Game(gameCode,gameKey);
 	
-//	public void initialize(BlackJackModel model) {
-//		InitDatabase.init();
-//		IDatabase db = DatabaseProvider.getInstance();
-//		pile.populate();
-//		pile.shuffle();
-//		
-//		
-//	
-//	public void hit() {
-//		IDatabase db = DatabaseProvider.getInstance();
-//		
-//	}
+	public BlackJackController(String gameCode, String gameKey) {
+		super(gameCode, gameKey);
+	}
+	public void initialize() {
+		mainPile.populate();
+		mainPile.shuffle();
+		
+		for(Player p : players) {
+			p.getPile().addCards(mainPile.removeCards(2));
+			p.getPile().setVisibleIndex(0);
+		}
+		db.createGame(blj);
+	}
+	
+	public void hit() {
+		Player p = players.get(tko.CurrentPlayer());
+		p.getPile().drawCard();
+		nextTurn();
+		db.updateGame(db.getGameIdFromGame(blj), blj);
+	}
 	
 	// for hold we want to basically skip a players turn
 	public void hold(BlackJackModel model) {
 		nextTurn();
+		db.updateGame(db.getGameIdFromGame(blj), blj);
 	}
 	
 	// for stay/freeze we want to skip a players turn until the other player calls stay
-	public void freeze(BlackJackModel model) {
-		//removePlayer();
+	public void freeze() {
+		
 	}
 	
 	// split needs to compare cards by rank and if they are the same then move cards to alt hand
 	public void split(BlackJackModel model) {
-		Rank one = ((StandardCard) model.getHand().getTopCard()).getRank();
-		Rank two = ((StandardCard) model.getHand().getCard(model.getHand().getIndexOfTopCard() + 1)).getRank();
-		if((model.getHand().getNumCards() == 2) && (one.equals(two))){
-			model.getAltHand().addCards(model.getHand().removeCards(1));
+		Player p = players.get(tko.CurrentPlayer());
+		Rank one = ((StandardCard) p.getPile().getCard(p.getPile().getIndexOfTopCard())).getRank();
+		Rank two = ((StandardCard) p.getPile().getCard(p.getPile().getIndexOfTopCard()+1)).getRank();
+		if((p.getPile().getNumCards() == 2) && (one.equals(two))){
+			p.getAltPile().addCard(p.getPile().getTopCard());
 		}
+		db.updateGame(db.getGameIdFromGame(blj), blj);
 	}
 	
-	public void checkWin(BlackJackModel model) {
-		
-	}
-	
-	public void checkBust(BlackJackModel model){
-		
+	public boolean checkWin() {
+		for(Player p : players) {
+			if(p.getPile().getValueStandard() == 21) {
+				return true;
+			}
+		}
+		return false;
+//		db.updateBlackjackStats(stat, user_id);
+//		db.updateGlobalStats(stat, user_id);
 	}
 
 }
