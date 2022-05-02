8fd55e6345fbada84ac61a6f272ed62d265f1bb3
pwalter1014 <pwalter3@ycp.edu>
2022-05-01 23:47:53 -0400

    started uno testing
    need to make more deterministic testing

--- Game Repository/src/Tests/UnoControllerTest.java
+++ Game Repository/src/Tests/UnoControllerTest.java
@@ -2,6 +2,7 @@ package Tests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.Color;
 import java.io.IOException;
 
 import org.junit.BeforeClass;
@@ -15,12 +16,12 @@ import Database.elves.PlayerAlreadyExistsException;
 import Models.BlackJackController;
 import Models.Game;
 import Models.Player;
+import Models.UnoCard;
 import Models.UnoController;
 
 public class UnoControllerTest {
 
 	private static Game model;
-	private static UnoController control;
 	private static Player three;
 	private static Player four;
 	private static Player five;
@@ -39,7 +40,6 @@ public class UnoControllerTest {
 		} catch (IOException e){
 		}
 		model = new Game(IDatabase.Key_Uno);
-		control = new UnoController();
 		three = new Player(true, db.createUser("booboo", "b"));
 		four = new Player(true, db.createUser("doodoo", "d"));
 		five = new Player(true, db.createUser("coocoo", "c"));
@@ -52,15 +52,16 @@ public class UnoControllerTest {
 		} catch(PlayerAlreadyExistsException e) {
 			
 		}
+		modelId = db.createGame(model);
 		try {
-			modelId = control.initialize(model);
+			UnoController.initialize(modelId);
 		} catch (Exception e) {
 		}
 		
 	}
 	
 	@Test
-	public void testGameSim() throws Exception {
+	public void testInit() throws Exception {
 		
 		//Test if initialize method works
 		model = db.getGameFromGameId(modelId);
@@ -72,5 +73,39 @@ public class UnoControllerTest {
 		//assertEquals(1, model.getPlayers().get(1).getPile().getVisibleIndex());
 		assertEquals(7, model.getPlayers().get(2).getPile().getNumCards());
 		assertEquals(7, model.getPlayers().get(3).getPile().getNumCards());
+	}
+	
+	@Test
+	public void testSkip() throws Exception{
+		UnoController.skip(modelId);
+		model = db.getGameFromGameId(modelId);
+		assertEquals(model.getTurnOrder().CurrentPlayer(), db.getPlayerIdFromPlayer(five));
+		
+		UnoController.skip(modelId);
+		model = db.getGameFromGameId(modelId);
+		assertEquals(model.getTurnOrder().CurrentPlayer(), db.getPlayerIdFromPlayer(three));
+	}
+	
+	@Test
+	public void testReverse() throws Exception{
+		UnoController.reverse(modelId);
+		model = db.getGameFromGameId(modelId);
+		assertEquals(model.getTurnOrder().CurrentPlayer(), db.getPlayerIdFromPlayer(six));
+	}
+	
+	@Test
+	public void testDrawTwo() throws Exception{
+		UnoController.drawTwo(modelId);
+		model = db.getGameFromGameId(modelId);
+		assertEquals(9, model.getPlayers().get(2).getPile().getNumCards());
+	}
+	
+	@Test
+	public void testDrawFour() throws Exception{
+		String color = "Blue";
+		UnoController.drawFour(modelId, color);
+		model = db.getGameFromGameId(modelId);
+		assertEquals(11, model.getPlayers().get(1).getPile().getNumCards());
+		assertEquals(Color.BLUE, ((UnoCard) model.getAltPile().getTopCard()).getColor());
 	}
 }
